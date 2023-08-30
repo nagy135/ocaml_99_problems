f8f473be44904dfa54bc5330b1c500a7f1e2b231
Viktor Nagy <viktor.nagy1995@gmail.com>
2023-08-30 10:40:01 +0200

    feat: first

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,18 @@
-let () = print_endline "Hello, World!"
+module First =
+  struct
+    let rec last list =
+      match list with
+        | [] -> None
+        | [x] -> Some x
+        | _ :: t -> last t
+  end ;;
+
+
+let () =
+  (* first *)
+  let open First in
+  let first_res = last ["1" ; "2" ; "3"] in
+  (* let first_res = last [] in *)
+  match first_res with 
+    | Some x -> print_endline x
+    | None -> print_endline "nothing"
